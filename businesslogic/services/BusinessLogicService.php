61b60139a4f0158eed9cd5843299fae16b7b742e
Lindsey DiLoreto <lindsey@doublesecretagency.com>
2016-03-10 22:02:27 -0800

    Removed extra whitespace

--- businesslogic/services/BusinessLogicService.php
+++ businesslogic/services/BusinessLogicService.php
@@ -8,14 +8,14 @@ namespace Craft;
 
 /**
  * Business Logic Service
- * 
+ *
  * When things get real (or at least really complex), then it's time to start using
  * the Service file. Some good reasons to use a Service method:
  *
  *     1. Your logic is super complex.
  *     2. You need to interact with the database.
  *     3. You want to call your service from some other plugin.
- * 
+ *
  * If you find yourself needing to tap into the database for whatever reason, that logic
  * belongs here. Whether you're approaching the database with a custom Record (not included),
  * or you need to write a custom query to do the job, this is the place to make it happen.
@@ -25,7 +25,7 @@ namespace Craft;
 
 class BusinessLogicService extends BaseApplicationComponent
 {
-	
+
 	/**
 	 * This function can literally be anything you want.
 	 *
